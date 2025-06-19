cd8b6e7b93accb3ac4162dc85e4ed62b7eee0ddd
fantasyalebar <bardotalexandre@gmail.com>
2025-06-19 16:09:17 +0700

    project finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from stats import get_num_words
-from stats import count_characters
+import sys
+from stats import get_num_words, count_characters, get_chars_for_report, print_report
+
 
 def get_book_text(books):
     with open(books) as f:
@@ -7,11 +8,13 @@ def get_book_text(books):
     return file_contents
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     num_word = get_num_words(book_text)
-    print(f"{num_word} words found in the document")
     letter_word = count_characters(book_text)
-    print(letter_word)
+    char_list = get_chars_for_report(letter_word)
+    print_report(num_word, char_list)
 main()
-
--- stats.py
+++ stats.py
@@ -17,3 +17,27 @@ def count_characters(text):
             # Le caractère n'existe pas encore, commence à 1
             char_count[lower_char] = 1
     return char_count
+
+
+def get_chars_for_report(char_counts_dict):
+    char_list = [] 
+    for char, num in char_counts_dict.items():
+        new_character_dict = {"char": char, "num": num}
+        char_list.append(new_character_dict)
+    return char_list
+
+def sort_on(dict):
+    return dict["num"]
+
+def print_report(num_word, char_list):
+    char_list.sort(reverse=True, key=sort_on)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_word} total words")
+    print("--------- Character Count -------")
+    for char_dict in char_list:
+        character = char_dict["char"]
+        if character.isalpha():
+            print(f"{character}: {char_dict['num']}")
+    print("============= END ===============")
